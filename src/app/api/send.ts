b7fe9e36fdb37d5c00904da86057e8de7bbe1280
David-konate <56818495+David-konate@users.noreply.github.com>
2024-06-27 21:58:34 +0200

    envoi mail ok.1.2

--- src/app/api/send.ts
+++ src/app/api/send.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Resend, CreateEmailOptions, SendEmailResponse } from 'resend';
+import { Resend } from 'resend';
 
 const resend = new Resend('re_Ju6UsAw3_9F2r4yhmSAknXwaMWZPJjbKz');
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+// Named arrow function
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   if (req.method === 'POST') {
-    const { emailData }: { emailData: CreateEmailOptions } = req.body;
+    const { emailData }: { emailData: any } = req.body;
 
     try {
-      const response: SendEmailResponse = await resend.emails.send(emailData);
+      const response = await resend.emails.send(emailData);
       res.status(200).json({ message: 'Email envoyé avec succès!', response });
     } catch (error) {
       res.status(500).json({ message: 'Échec de l\'envoi de l\'email', error });
@@ -17,3 +18,5 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
     res.status(405).json({ message: 'Méthode non autorisée' });
   }
 };
+
+export default handler; // Export the named function as default
