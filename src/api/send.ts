25c7a7d46a1ed7a2581426457939883e735f10c1
David-konate <56818495+David-konate@users.noreply.github.com>
2024-06-27 21:53:46 +0200

    envoi mail ok.1

--- src/api/send.ts
+++ src/api/send.ts
@@ -1,14 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Resend, CreateEmailOptions, SendEmailResponse } from 'resend';
+import { Resend } from 'resend';
 
 const resend = new Resend('re_Ju6UsAw3_9F2r4yhmSAknXwaMWZPJjbKz');
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   if (req.method === 'POST') {
-    const { emailData }: { emailData: CreateEmailOptions } = req.body;
+    const { emailData }: { emailData } = req.body;
 
     try {
-      const response: SendEmailResponse = await resend.emails.send(emailData);
+      const response = await resend.emails.send(emailData);
       res.status(200).json({ message: 'Email envoyé avec succès!', response });
     } catch (error) {
       res.status(500).json({ message: 'Échec de l\'envoi de l\'email', error });
